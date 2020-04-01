158d8537bd52805fc290cde75bdd16fc1c55f71f
xiaofeng0411 <824316775@qq.com>
2020-04-01 19:53:55 +0800

    commit

---
+++ init_test.py
@@ -0,0 +1,10 @@
+'''
+@Author: huxiaofeng
+@Date  : 2020-03-30 10:14
+'''
+
+#__init__.py 让一个文件夹变成python包，然后才可以通过文件夹饮用里边的module
+# 初始化工作
+
+def walk():
+    print('多走路')
--- thread_test/thread_test.py
+++ thread_test/thread_test.py
@@ -8,11 +8,13 @@ import time
 
 
 def run(n):
-    print("task",n)
+    print("task", n)
     time.sleep(2)
 
 
-t1 = threading.Thread(target=run,args=("t1",))
-t2 = threading.Thread(target=run,args=("t2",))
-t1.start()
-t2.start()
+start_time = time.time()
+for i in range(50):
+    t = threading.Thread(target=run, args=("t-%s"%i,))
+    t.start()
+
+print("cost:", time.time()-start_time)
