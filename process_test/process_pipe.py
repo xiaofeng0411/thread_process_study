179dc6a4e521c019eae3eec2739eb12be4697b8b
xiaofeng0411 <824316775@qq.com>
2020-04-03 22:16:58 +0800

    implement interprocess communication by process pipe

---
+++ process_test/process_pipe.py
@@ -0,0 +1,19 @@
+'''
+@Author: huxiaofeng
+@Date  : 2020-04-03 22:08
+'''
+
+from multiprocessing import Process,Pipe
+
+
+def f(conn):
+    conn.send([42, None, 'hello'])
+    conn.close()
+
+
+if __name__ == "__main__":
+    parent_conn ,child_conn = Pipe()
+    p = Process(target=f,args=(child_conn,))
+    p.start()
+    print(parent_conn.recv())
+    p.join()
