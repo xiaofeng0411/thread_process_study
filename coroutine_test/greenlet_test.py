8d192a4b0ab5b5ed1e1ad3aca28783d5c06ac6ed
xiaofeng0411 <824316775@qq.com>
2020-04-04 17:21:13 +0800

    add greenlet code

---
+++ coroutine_test/greenlet_test.py
@@ -0,0 +1,25 @@
+'''
+@Author: huxiaofeng
+@Date  : 2020-04-04 17:14
+'''
+
+
+from greenlet import greenlet
+
+
+def test1():
+    print(12)
+    gr2.switch()
+    print(34)
+    gr2.switch()
+
+
+def test2():
+    print(56)
+    gr1.switch()
+    print(78)
+
+
+gr1 = greenlet(test1)#启动一个协程
+gr2 = greenlet(test2)
+gr1.switch()
