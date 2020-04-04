12f94ebed9aeea5eefb2be8e0e501a9264ecb169
xiaofeng0411 <824316775@qq.com>
2020-04-04 09:48:34 +0800

    add process lock code

--- process_test/process_lock.py
+++ process_test/process_lock.py
@@ -2,3 +2,20 @@
 @Author: huxiaofeng
 @Date  : 2020-04-04 09:33
 '''
+
+
+from multiprocessing import Process,Lock
+
+
+def f(l,i):
+    l.acquire()
+    try:
+        print('hello world',i)
+    finally:
+        l.release()
+
+
+if __name__ == '__main__':
+    lock = Lock()
+    for num in range(10):
+        Process(target=f,args=(lock,num)).start()
